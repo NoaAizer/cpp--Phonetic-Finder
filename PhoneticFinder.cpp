4f0c4dcce5fea65eeace76acb34dbc312f1f0922
Noa <noa.aizer@gmail.com>
2020-03-29 13:38:55 +0300

    fix a bug

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -1,7 +1,9 @@
-#include "PhoneticFinder.hpp"
+
 #include <iostream>
 #include <string>
 #include<vector>
+#include "PhoneticFinder.hpp"
+
 using namespace std;
 
 namespace phonetic {
@@ -24,12 +26,13 @@ vector<string> split(string str, string token){
           if(index!=str.size()-1){
             int nextChar= str.find(str.substr(index+1,str.size()));
             if(index!=nextChar){
-            result.push_back(str.substr(0,index));
-            str = str.substr(index+token.size());
+              result.push_back(str.substr(0,index));
+              str = str.substr(index+token.size());
             }
           }
-            if(str.size()==0)result.push_back(str);
-        }else{
+          if(str.size()==0)result.push_back(str);
+        }
+        else{
             result.push_back(str);
             str = "";
         }
@@ -43,7 +46,7 @@ vector<string> split(string str, string token){
   if(wordOrg.size()!=wordTextOrg.size())return false;
   std::string word=toLowerCase(wordOrg);
   std::string wordText=toLowerCase(wordTextOrg);
-  for(int i=0; i<word.size();++i)
+  for(int i=word.find_first_not_of (' '); i<word.size();++i)
     {
       if(word[i]!=wordText[i]){
         if(word[i] == 'v' || word[i] == 'w'){
@@ -55,50 +58,49 @@ vector<string> split(string str, string token){
         else if(word[i] == 'c' || word[i] == 'k' || word[i] == 'q'){
           if(wordText[i]!='c' && wordText[i]!='k' & wordText[i]!='q') flag= false;
         }
-      else if(word[i] == 'j' || word[i] == 'g'){
-      if(wordText[i]!='j' && wordText[i]!='g') flag= false;
-    }
-      else if(word[i] == 's' || word[i] == 'z'){
-      if(wordText[i]!='s' && wordText[i]!='z') flag= false;
-    }
-      else if(word[i] == 'd' || word[i] == 't'){
-      if(wordText[i]!='d' && wordText[i]!='t') flag= false;
-    }
-      else if(word[i] == 'o' || word[i] == 'u'){
-      if(wordText[i]!='o' && wordText[i]!='u') flag= false;
-    }
-      else if(word[i] == 'i' || word[i] == 'y'){
-       if(wordText[i]!='i' && wordText[i]!='y') flag= false;
-     }
-  }
-  
+        else if(word[i] == 'j' || word[i] == 'g'){
+          if(wordText[i]!='j' && wordText[i]!='g') flag= false;
+        }
+        else if(word[i] == 's' || word[i] == 'z'){
+          if(wordText[i]!='s' && wordText[i]!='z') flag= false;
+        }
+        else if(word[i] == 'd' || word[i] == 't'){
+          if(wordText[i]!='d' && wordText[i]!='t') flag= false;
+        }
+        else if(word[i] == 'o' || word[i] == 'u'){
+          if(wordText[i]!='o' && wordText[i]!='u') flag= false;
+        }
+        else if(word[i] == 'i' || word[i] == 'y'){
+          if(wordText[i]!='i' && wordText[i]!='y') flag= false;
+        }
+        else if (word[i]!=wordText[i]) flag=false;
+      }
     }
   return flag;
   }
 
 	std::string find(std::string text, std::string word){
+    
     std::string *pw=&word;
     std::string *pt=&text;
 
-//(word.find_first_not_of (' ') != std::string::npos)
     if(word.empty()  || (pw == NULL) || (word.find_first_not_of (' ') == std::string::npos))
       throw std::runtime_error {"The "+ text+" is invalid"};
     if(text.empty() || (pt == NULL) || (text.find_first_not_of (' ') == std::string::npos))
       throw std::runtime_error {"The "+ word+" is invalid word"};
 
-    // if(text.find(word)!= std::string::npos)
-    //   return word;
-    
-    vector<string> wordsFromText = split(text ," ");
 
+    vector<string> wordsFromText = split(text ," ");
 
     for ( const auto& item : wordsFromText) {
       if(checkWords(word,item))
-        return item;    
+        return item; 
     }
-//{"The word "+ word+" doesn't appear in the text"}
- // throw std::exception() ;
+
 throw runtime_error("Did not find the word '"+word+"' in the text");
+
+return NULL;
   }
+
 }
 
